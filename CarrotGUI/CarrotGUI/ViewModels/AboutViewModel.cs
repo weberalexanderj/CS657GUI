5c66ca41de269f49feec79b7f961c18aafdf2992
Alex Weber <weberalexanderj@gmail.com>
2018-04-17 16:18:38 -0500

    initial commit

---
+++ CarrotGUI/CarrotGUI/ViewModels/AboutViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MvvmDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarrotGUI.ViewModels
+{
+    class AboutViewModel : ViewModelBase, IModalDialogViewModel
+    {
+        public bool? DialogResult { get { return false; } }
+
+        public string Content
+        {
+            get
+            {
+                return "CarrotGUI" + Environment.NewLine +
+                        "Created by Geno" + Environment.NewLine +
+                        "Address" + Environment.NewLine +
+                        "2018";
+            }
+        }
+
+        public string VersionText
+        {
+            get
+            {
+                var version1 = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+                // For external assemblies
+                // var ver2 = typeof(Assembly1.ClassOfAssembly1).Assembly.GetName().Version;
+                // var ver3 = typeof(Assembly2.ClassOfAssembly2).Assembly.GetName().Version;
+
+                return "CarrotGUI v" + version1.ToString();
+            }
+        }
+    }
+}
